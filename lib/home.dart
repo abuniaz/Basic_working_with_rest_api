e9cd4267b9e4d4f9c5a7e0e9132fa37921b7dabf
Abu Niaz <niaz35-507@diu.edu.bd>
2022-08-29 14:54:39 +0600

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -19,6 +19,7 @@ class _MyHomePageState extends State<MyHomePage> {
         await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
     var data = jsonDecode(response.body.toString());
     if (response.statusCode == 200) {
+      postList.clear();
       for (Map i in data) {
         postList.add(Postmodel.fromJson(i));
       }
@@ -51,9 +52,25 @@ class _MyHomePageState extends State<MyHomePage> {
                             padding: const EdgeInsets.all(8.0),
                             child: Column(
                                 mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
+                                  const Text(
+                                    'Title',
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold),
+                                  ),
                                   Text(postList[index].title.toString()),
-                                  Text(postList[index].title.toString()),
+                                  const SizedBox(
+                                    height: 5,
+                                  ),
+                                  const Text(
+                                    'Description',
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(postList[index].body.toString()),
                                 ]),
                           ),
                         );
