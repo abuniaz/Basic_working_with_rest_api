3639b935d7a259f294204946778e35cc565d2ed1
Abu Niaz <niaz35-507@diu.edu.bd>
2022-08-30 18:29:47 +0600

    Create example_two.dart

---
+++ lib/example_two.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class ExampleTwo extends StatefulWidget {
+  const ExampleTwo({Key? key}) : super(key: key);
+
+  @override
+  State<ExampleTwo> createState() => _ExampleTwoState();
+}
+
+class _ExampleTwoState extends State<ExampleTwo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Api Course'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
