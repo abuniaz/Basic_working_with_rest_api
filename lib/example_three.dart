9352568f64297d1f56689a519b787ef268dc4db4
Abu Niaz <niaz35-507@diu.edu.bd>
2022-09-03 17:52:28 +0600

    Update example_three.dart

--- lib/example_three.dart
+++ lib/example_three.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:rest_api_with_null_safety/model/user_model.dart';
+import 'package:http/http.dart' as http;
 
 class ExampleThree extends StatefulWidget {
   const ExampleThree({Key? key}) : super(key: key);
@@ -8,6 +12,20 @@ class ExampleThree extends StatefulWidget {
 }
 
 class _ExampleThreeState extends State<ExampleThree> {
+  List<UserModel> userList = [];
+
+  Future<List<UserModel>> getUserApi() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      for (Map i in data) {}
+      return userList;
+    } else {
+      return userList;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
