782b136d89d58ea5176349852bd075adf16f23f8
Abu Niaz <niaz35-507@diu.edu.bd>
2022-09-04 19:57:13 +0600

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -45,41 +45,40 @@ class _MyHomePageState extends State<MyHomePage> {
                   return const Text('Loading');
                 } else {
                   return ListView.builder(
-                      itemCount: postList.length,
-                      itemBuilder: ((context, index) {
-                        return Card(
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const Text(
-                                  'Title',
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                Text(
-                                  postList[index].title.toString(),
-                                ),
-                                const SizedBox(
-                                  height: 5,
-                                ),
-                                const Text(
-                                  'Description',
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                Text(
-                                  postList[index].title.toString(),
-                                ),
-                              ],
-                            ),
+                    itemCount: postList.length,
+                    itemBuilder: ((context, index) {
+                      return Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                'Title',
+                                style: TextStyle(
+                                    fontSize: 15, fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                postList[index].title.toString(),
+                              ),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              const Text(
+                                'Description',
+                                style: TextStyle(
+                                    fontSize: 15, fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                postList[index].title.toString(),
+                              ),
+                            ],
                           ),
-                        );
-                      }));
+                        ),
+                      );
+                    }),
+                  );
                 }
               },
             ),
